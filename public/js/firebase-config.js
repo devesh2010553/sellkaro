23f4f7969739f66ea756959d967548b985d9155c
devesh2010553 <133110094+devesh2010553@users.noreply.github.com>
2026-06-23 19:27:09 +0530

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -4,12 +4,13 @@
 // (Firebase Console -> Project Settings -> General -> Your apps -> Web app)
 // ============================================================
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDE4fEeABu_m8vVFyhOAISdMnz3KGlS-Rc",
+  authDomain: "vertex-2d145.firebaseapp.com",
+  projectId: "vertex-2d145",
+  storageBucket: "vertex-2d145.firebasestorage.app",
+  messagingSenderId: "628055020171",
+  appId: "1:628055020171:web:0fe92aed4cf86f3416fa8e",
+  measurementId: "G-PL3R9JWYN4"
 };
 
 firebase.initializeApp(firebaseConfig);
